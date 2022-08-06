3f84245b74ec61b92851bfaa011403d44bc9393d
Shahnaz <ekshahnaz@gmail.com>
2022-08-06 20:16:34 +0530

    Initial Commit

---
+++ PomodoroSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomodoroSwiftUI
+//
+//  Created by Shahnaz EK on 06/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PomodoroSwiftUI/PomodoroSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroSwiftUIApp.swift
+//  PomodoroSwiftUI
+//
+//  Created by Shahnaz EK on 06/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
